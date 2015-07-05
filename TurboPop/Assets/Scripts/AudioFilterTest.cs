3e2febc58c6e105b5db18c02391806c983c101b1
Rob Butler <robert@silverbackgames.com>
2015-07-04 23:04:27 -0300

    some rough audio synchronization

---
+++ TurboPop/Assets/Scripts/AudioFilterTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFilterTest : MonoBehaviour {
+
+	public float currentValue;
+
+	void OnAudioFilterRead(float[] data, int channels){
+		float myValue = 0;
+		float myValue2 = 0;
+		for (int i = 0; i < data.Length * .3; i += channels){
+			myValue += Mathf.Abs(data[i]);
+		}
+
+		for (int i = 1; i < data.Length * .3 - 1; i += channels){
+			myValue2 += Mathf.Abs(data[i]);
+		}
+		currentValue = Mathf.Max(myValue, myValue2);
+	}
+}
---
+++ TurboPop/Assets/Scripts/ReactToAudioTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactToAudioTest : MonoBehaviour {
+
+	static AudioFilterTest audioFilterTest;
+
+	float thresholdForResponse = 90;
+
+	bool rotating = false;
+
+	Vector3 rotation;
+
+	void Awake(){
+		rotation = transform.localRotation.eulerAngles;
+		if (audioFilterTest == null){
+			audioFilterTest = GameObject.FindObjectOfType<AudioFilterTest>();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (audioFilterTest.currentValue >= thresholdForResponse && !rotating){
+			transform.Rotate(0,2,0);
+		}
+	}
+
+}
