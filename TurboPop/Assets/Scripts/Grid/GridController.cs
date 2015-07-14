e6014074a2b409b0720d58bd1154e11c0c547030
Rob Butler <robert@silverbackgames.com>
2015-07-14 20:19:03 -0300

    starting on grid classes

--- TurboPop/Assets/Scripts/Grid/GridController.cs
+++ TurboPop/Assets/Scripts/Grid/GridController.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class GridController : MonoBehaviour {
 
+	static GridController instance;
+
+	public static GridController Instance{
+		get {
+			return instance;
+		}
+	}
+
+	GridSegment[] gridSegments;
+
+	void Awake(){
+		if (instance == null){
+			instance = this;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
